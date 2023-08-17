1d101c4f6cdd309adeb1fb89a8546efe08638ba0
chintanrout-refyne <142498239+chintanrout-refyne@users.noreply.github.com>
2023-08-17 16:53:17 +0530

    Add files via upload

---
+++ template/app.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request
+import csv
+import json
+import requests
+import sys  # Import the sys module
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    csv_file = request.files['csvFile']
+    bearer_token = request.form['bearerToken']
+
+    # Set the endpoint URL
+    url = "https://api.beta.prod.refyne.co.in/refyne-admin/emi-repayment"
+
+    # Set the headers
+    headers = {
+        'accept': '*/*',
+        'Authorization': f'Bearer {bearer_token}',
+        'Content-Type': 'application/json'
+    }
+
+    with open(csv_file, 'r') as file:
+        csv_reader = csv.DictReader(file)
+        for row in csv_reader:
+            payload = {
+                "userId": row['UserID'],
+                "emiAmount": int(row['emiAmount']),
+                "paidAt": "2023-08-16T20:55:24.953Z"
+            }
+            
+            response = requests.post(url, headers=headers, data=json.dumps(payload))
+            
+            if response.status_code == 200:
+                print(f"Successfully sent EMI repayment for UserID: {row['UserID']}")
+            else:
+                print(f"Failed to send EMI repayment for UserID: {row['UserID']}, Status Code: {response.status_code}")
+                sys.exit(1)  # Exit the script with a non-zero status code
+
+    return "CSV data uploaded and processed."
+
+if __name__ == '__main__':
+    app.run(debug=True)
